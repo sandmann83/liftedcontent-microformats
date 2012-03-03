992fa730728533c288035e343660aeb725ebcf6f
Stefan Bradl <stefan_bradl@web.de>
2012-03-03 17:04:45 +0100

    hAtom updated

--- src/main/scala/eu/sbradl/liftedcontent/microformats/snippet/Atom.scala
+++ src/main/scala/eu/sbradl/liftedcontent/microformats/snippet/Atom.scala
@@ -6,6 +6,7 @@ import scala.xml.NodeSeq
 object Atom {
 
   def feed = "data-mf=feed [class+]" #> "hfeed"
+  def feed(selector: String) = "%s [class+]".format(selector) #> "hfeed"
   
   def entry = "data-mf=entry [class+]" #> "hentry"
   def entry(selector: String) = "%s [class+]".format(selector) #> "hentry"
@@ -14,10 +15,21 @@ object Atom {
   def title(selector: String) = "%s [class+]".format(selector) #> "entry-title"
   
   def content = "data-mf=content [class+]" #> "entry-content"
+  def content(selector: String) = "%s [class+]" #> "#entry-content"
   
-  def date(d: String) = "data-mf=date [class+]" #> "published" & "data-mf=data [title]" #> d
-  def date(selector: String, d: String) = "%s [class+]".format(selector) #> "published" & "%s [title]".format(selector) #> d
+  def summary = "data-mf=summary [class+]" #> "entry-summary"
+  def summary(selector: String) = "%s [class+]" #> "#entry-summary"
   
+  def updated(d: String) = "data-mf=updated [class+]" #> "updated" & "data-mf=updated [title]" #> d
+  def updated(selector: String, d: String) = "%s [class+]".format(selector) #> "updated" & "%s [title]".format(selector) #> d
+  
+  def published(d: String) = "data-mf=date [class+]" #> "published" & "data-mf=date [title]" #> d
+  def published(selector: String, d: String) = "%s [class+]".format(selector) #> "published" & "%s [title]".format(selector) #> d
+  
+  def author = "data-mf=author [class+]" #> "author"
   def author(selector: String) = "%s [class+]".format(selector) #> "author"
+  
+  def bookmark = "data-mf=bookmark [rel]" #> "bookmark"
+  def bookmark(selector: String) = "%s [rel]".format(selector) #> "bookmark"
   
 }
